3d031fe2b417e41d1e461ed2d9977b73f121ebde
theBashShell <edohbruno@gmail.com>
2020-09-25 22:52:35 +0000

    file extension tests

--- go/uptimer/uptimer.go
+++ go/uptimer/uptimer.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		fmt.Println(req.UserAgent())
+		fmt.Println(strings.ToLower(req.Method))
+		fmt.Println(req.Proto)
 
+		res.WriteHeader(404)
 		res.Write([]byte("hello"))
 	})
 
